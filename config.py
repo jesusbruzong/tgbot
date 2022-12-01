486d7e6b7fa120445db67a7a148bd99bf5703c6c
jesusbruzong <47755683+jesusbruzong@users.noreply.github.com>
2022-12-01 22:44:27 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+telegram_token = "5751454931:AAHZOgjIApv84n1haoOSKcvgyOEUXcDS_VA"
+chat_id = 657490996
+
+#token macflys = 5942053794:AAG8sAEKFoWLW0Cy9-vpsvO_Mbqz3i6z5vY
+
+#token pruebas = 5751454931:AAHZOgjIApv84n1haoOSKcvgyOEUXcDS_VA
